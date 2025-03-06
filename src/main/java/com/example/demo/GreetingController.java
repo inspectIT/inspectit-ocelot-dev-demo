1abf48bbab3f825dc708b0aca7930759d31456fc
EddeCCC <eduard.schander@gmx.de>
2025-03-06 16:35:49 +0100

    add dependency

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -6,7 +6,6 @@ import com.example.demo.service.RandomService;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
